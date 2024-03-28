81636ed81bd20abe84be48a6e12c0cec9225111a
PawelManczak <pawel14522@gmail.com>
2024-03-28 17:47:39 +0100

    lab 3 exercise 4

---
+++ LAB3/4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using klasanet;
+
+namespace KSRL3E4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKlasa klasa = new KlasaClass();
+            klasa.Test("zadanie 4 dziala");
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
+
