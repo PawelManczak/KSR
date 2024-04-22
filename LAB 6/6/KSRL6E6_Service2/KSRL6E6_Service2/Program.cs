67315a4271f0b3421368eaf7b9233f3d7e52b5e3
PawelManczak <pawel14522@gmail.com>
2024-04-22 14:52:48 +0200

    service 2

---
+++ LAB 6/6/KSRL6E6_Service2/KSRL6E6_Service2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSRL6E6_Service2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new ServiceHost(typeof(Zadanie6));
+
+            host.AddServiceEndpoint(
+                typeof(IZadanie6),
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/exercise6-service2");
+
+            host.Open();
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+
+    [ServiceContract]
+    public interface IZadanie6
+    {
+        [OperationContract]
+        int Dodaj(int a, int b);
+    }
+    public class Zadanie6 : IZadanie6
+    {
+        public int Dodaj(int a, int b)
+        {
+            Console.WriteLine("sewis zapasowy");
+            // for tests reasons
+            return a * b;
+        }
+    }
+
+
+
+}
