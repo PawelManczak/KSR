b513960a95956f06c771a1c1f308bcd7f5e5611e
PawelManczak <pawel14522@gmail.com>
2024-04-22 14:49:52 +0200

    exercise 2

---
+++ LAB 6/1&2/2/KSRL6E2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace KSRL6E2
+{
+    [ServiceContract]
+    public interface IZadanie1
+    {
+        [OperationContract]
+        string ScalNapisy(string a, string b);
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zad2
+            var discoveryClient = new DiscoveryClient(
+                new UdpDiscoveryEndpoint("soap.udp://localhost:30703"));
+
+            var lst = discoveryClient.Find(new FindCriteria(typeof(IZadanie1))).Endpoints;
+
+            discoveryClient.Close();
+
+            if (lst.Count > 0)
+            {
+                var addr = lst[0].Address;
+                var proxy = ChannelFactory<IZadanie1>
+                    .CreateChannel(new NetNamedPipeBinding(), addr);
+                Console.WriteLine(proxy.ScalNapisy("exercise 2", " is working 188756"));
+                Console.ReadKey();
+                ((IDisposable)proxy).Dispose();
+
+                
+            }
+        }
+    }
+}
