b97d4eb1c57dda7d45244cdda2975e7b89425669
PawelManczak <pawel14522@gmail.com>
2024-04-23 08:31:13 +0200

    exercise 6 - client

---
+++ LAB 6/6/KSRL6E6-client/KSRL6E6-client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSRL6E6_client
+{
+    [ServiceContract]
+    public interface IZadanie6
+    {
+        [OperationContract]
+        int Dodaj(int a, int b);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fac = new ChannelFactory<IZadanie6>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress("net.pipe://localhost/router")
+            );
+
+            var client = fac.CreateChannel();
+
+            Console.WriteLine(client.Dodaj(21, 38));
+
+            Console.ReadKey();
+
+            ((IDisposable)client).Dispose();
+
+            fac.Close();
+        }
+    }
+}
