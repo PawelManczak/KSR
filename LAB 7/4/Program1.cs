e25fefb0d5b48e3ee92c8f08f13bdef535d08688
PawelManczak <pawel14522@gmail.com>
2024-04-29 22:46:45 +0200

    exercise 4

---
+++ LAB 7/4/Program1.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSRl7E1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory()
+            {
+                UserName = "zyfkaljc",
+                Password = "i9rkwiZr1uYcU1abPKWEccn6NpjuDihg",
+                HostName = "cow.rmq2.cloudamqp.com",
+                VirtualHost = "zyfkaljc"
+
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("message_queue", false, false, false, null);
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    // ex 3
+                    var headers = new Dictionary<string, object>
+                    {
+                        {"Header1", "188756"},
+                        {"Header2", i}
+                    };
+                    var properties = channel.CreateBasicProperties();
+                    properties.Headers = headers;
+
+                    string message = $"Wiadomość {i}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "message_queue", basicProperties: properties, body: body);
+                    Console.WriteLine($"Wysłano: {message}");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
