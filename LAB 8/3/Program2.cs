7d3e25f62617da2f81b616a68475fe0cb4fed25c
PawelManczak <pawel14522@gmail.com>
2024-05-06 19:21:55 +0200

    exercise 3

---
+++ LAB 8/3/Program2.cs
@@ -0,0 +1,48 @@
+﻿using KSRL8Komunikaty;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSRL8E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+            });
+
+            bus.Start();
+
+            Console.WriteLine("Wydawca wystartował");
+
+            Console.WriteLine("Naciśnij dowolny klawisz, aby wysłać komunikat...");
+            Console.ReadKey();
+
+            for (int i = 0; i <10; i++)
+            {
+                bus.Publish(new Komunikat() { Tekst = $"Treść komunikatu od wydawcy {i}" }, 
+                    ctx => { ctx.Headers.Set("klucz1", $"wartosc1 {i}");
+                        ctx.Headers.Set("klucz2", $"wartosc2 {i}");
+
+                    });
+            }
+            
+
+            Console.WriteLine("Wysłano komunikat");
+
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+}
