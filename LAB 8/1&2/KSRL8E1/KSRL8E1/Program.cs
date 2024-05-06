db10ebce18e7f78d40871835cd0e4f8355c2506b
PawelManczak <pawel14522@gmail.com>
2024-05-06 14:03:16 +0200

    exercise 1 & 2

---
+++ LAB 8/1&2/KSRL8E1/KSRL8E1/Program.cs
@@ -0,0 +1,44 @@
+﻿using KSRL8Komunikaty;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSRL8E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+            });
+
+            bus.Start();
+
+            Console.WriteLine("Wydawca wystartował");
+
+            Console.WriteLine("Naciśnij dowolny klawisz, aby wysłać komunikat...");
+            Console.ReadKey();
+
+            for (int i = 0; i <10; i++)
+            {
+                bus.Publish(new Komunikat() { Tekst = $"Treść komunikatu od wydawcy {i}" });
+            }
+            
+
+            Console.WriteLine("Wysłano komunikat");
+
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+}
---
+++ LAB 8/1&2/KSRL8E2/KSRL8E2/Program.cs
@@ -0,0 +1,42 @@
+﻿using KSRL8Komunikaty;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSRL8E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host(new Uri("rabbitmq://localhost"), cfg =>
+                {
+                    cfg.Username("guest");
+                    cfg.Password("guest");
+                });
+
+                sbc.ReceiveEndpoint("recvqueue", ep => {
+                    ep.Handler<Komunikat>(Handle);
+                });
+
+            });
+
+            bus.Start();
+
+            Console.WriteLine("Odbiorca wystartował. Oczekiwanie na wiadomości...");
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+
+        static async Task Handle(ConsumeContext<Komunikat> ctx)
+        {
+            await Console.Out.WriteLineAsync($"Odebrano: {ctx.Message.Tekst}");
+        }
+    }
+}
