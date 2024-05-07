65fd33693b0c531d66923504ad67b679e58732e0
PawelManczak <pawel14522@gmail.com>
2024-05-07 12:47:14 +0200

    exercise 6

---
+++ LAB 8/6/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSRL8Komunikaty
+{
+    public interface IKomunikat
+    {
+        string Tekst { get; set; }
+    }
+
+    public interface IDrugiTypKomunikatu
+    {
+        string TekstDrugiTyp { get; set; }
+    }
+
+    public interface IKomunikat3 : IKomunikat, IDrugiTypKomunikatu
+    {
+        string TekstTrzeciTyp { get; set; }
+    }
+}
---
+++ LAB 8/6/Program.cs
@@ -0,0 +1,64 @@
+﻿using KSRL8Komunikaty;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSRL8E1
+{
+    class Komunikat : IKomunikat
+    {
+        public string Tekst { get; set; }
+    }
+
+    class Komunikat2 : IDrugiTypKomunikatu
+    {
+        public string TekstDrugiTyp { get; set; }
+    }
+
+    class Komunikat3 : IKomunikat3
+    {
+        public string TekstTrzeciTyp { get; set; }
+        public string Tekst { get; set; }
+        public string TekstDrugiTyp { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+            });
+
+            bus.Start();
+
+            Console.WriteLine("Wydawca wystartował");
+
+            Console.WriteLine("Naciśnij dowolny klawisz, aby wysłać komunikaty...");
+            Console.ReadKey();
+
+            for (int i = 0; i < 10; i++)
+            {
+                bus.Publish<IKomunikat>(new Komunikat() { Tekst = $"Treść komunikatu od wydawcy {i}" });
+                Console.WriteLine($"wysłano: Treść komunikatu od wydawcy {i}");
+                bus.Publish<IDrugiTypKomunikatu>(new Komunikat2() { TekstDrugiTyp = $"Treść drugiego typu komunikatu {i}" });
+                Console.WriteLine($"wysłano: Treść drugiego typu komunikatu {i}");
+                bus.Publish<IKomunikat3>(new Komunikat3 { Tekst = $"K3 - tekst typ 1 {i}", TekstDrugiTyp = $"K3 - tekst typ 2 {i}", TekstTrzeciTyp = $"K3 - tekst typ 3 {i}" });
+                Console.WriteLine($"wysłano: Treść trzeciego typu komunikatu {i}");
+            }  
+
+            Console.WriteLine("Wysłano komunikaty");
+
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+}
