ebb6b7d124dd8ce4d2e81eb1c7e81b6f087cbf9b
PawelManczak <pawel14522@gmail.com>
2024-05-21 15:21:52 +0200

    controller changes

--- LAB 9/1/Program.cs
+++ LAB 9/1/Program.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using MassTransit;
-using System;
-using System.Threading.Tasks;
+using shared;
 
-namespace KSRL9Controller
+namespace Controller
 {
-    
-
-    public class Controller
+    class Program
     {
         static async Task Main(string[] args)
         {
@@ -24,25 +18,27 @@ namespace KSRL9Controller
                     h.Username("zyfkaljc");
                     h.Password("i9rkwiZr1uYcU1abPKWEccn6NpjuDihg");
                 });
-
-                cfg.Message<Ustaw>(x => x.SetEntityName("control_queue"));
             });
 
             await busControl.StartAsync();
+
             try
             {
                 while (true)
                 {
-                    var key = Console.ReadKey(true).Key;
-                    if (key == ConsoleKey.S)
+                    var key = Console.ReadKey().Key;
+                    string iv = Guid.NewGuid().ToString();
+
+                    switch (key)
                     {
-                        await busControl.Publish(new Ustaw { Dziala = true });
-                        Console.WriteLine("Wysłano polecenie: Start");
-                    }
-                    else if (key == ConsoleKey.T)
-                    {
-                        await busControl.Publish(new Ustaw { Dziala = false });
-                        Console.WriteLine("Wysłano polecenie: Stop");
+                        case ConsoleKey.S:
+                            await busControl.Publish(new Ustaw { Dziala = "True" });
+                            Console.WriteLine("Wysylanie komendy start");
+                            break;
+                        case ConsoleKey.T:
+                            await busControl.Publish(new Ustaw { Dziala = "False" });
+                            Console.WriteLine("Wysylanie komendy stop");
+                            break;
                     }
                 }
             }
@@ -52,10 +48,4 @@ namespace KSRL9Controller
             }
         }
     }
-
-    public class Ustaw
-    {
-        public bool Dziala { get; set; }
-    }
-
 }
