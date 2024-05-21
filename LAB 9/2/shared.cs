22aa52c66bd6baafd5e0939d975231fe0130af1e
PawelManczak <pawel14522@gmail.com>
2024-05-21 15:22:40 +0200

    Create shared.cs

---
+++ LAB 9/2/shared.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared
+{
+    public class Publ
+    {
+        public int Number { get; set; }
+    }
+
+    public class Ustaw
+    {
+        public string Dziala { get; set; }
+    }
+}
+
